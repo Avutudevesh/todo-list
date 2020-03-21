624a8a5ee8eb6baf4021116de4ae2073c2a44ced
Devesh Avutu <devesh14may@gmail.com>
2020-03-21 12:02:16 +0530

    added functionality and styles for todo website

--- app.js
+++ app.js
@@ -2,37 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-var today = new Date();
-
+var items = [];
 app.get("/", function(req, res) {
-	var day = today.getDay();
-	var weekDay = "";
-	switch (day) {
-		case 0:
-			weekDay = "Sunday";
-			break;
-		case 1:
-			weekDay = "Monday";
-			break;
-		case 2:
-			weekDay = "Tuesday";
-			break;
-		case 3:
-			weekDay = "Wednesday";
-			break;
-		case 4:
-			weekDay = "Thursday";
-			break;
-		case 5:
-			weekDay = "Friday";
-			break;
-		case 6:
-			weekDay = "Saturday";
-			break;
-	}
-	res.render("list", { dayOfWeek: weekDay });
+	res.render("list", { todoItems: items });
+});
+
+app.post("/", function(req, res) {
+	items.push(req.body.todoItem);
+	res.redirect("/");
 });
 
 app.listen(3001, function() {
